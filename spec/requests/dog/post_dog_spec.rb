8cdbacc89b242383aa2334113c8e1bbbc0708afe
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-06-12 10:25:07 -0700

    add test for post request returning name of dog, expect to fail.

---
+++ spec/requests/dog/post_dog_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "post a review route", :type => :request do 
+
+  before do 
+    post '/dogs', params: {:name => 'Jimmy', :age => 3, :description => 'Very energetic and loving, he enjoys going on walks and playing fetch', :breed => 'Husky'} 
+  end
+
+  it 'returns the dogs name ' do
+    expect(JSON.parse(response.body)['name']).to eq()
+  end
+  
+end
