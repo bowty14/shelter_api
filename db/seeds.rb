e7f5fef8138df7600440eee6d188d66f537a81ac
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-06-12 08:51:41 -0700

    update seed.rb file to seed the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin 
+    seed = Seed.new
+    seed.generate_dogs
+    seed.generate_cats
+  end
+
+  def generate_dogs
+    20.times do |i|
+      dog = Dog.create!(
+        name: Faker::Creature::Dog.name,
+        age: Faker::Number.between(from: 1, to: 10),
+        description: Faker::Hipster.sentence,
+        breed: Faker::Creature::Dog.breed
+      )
+      puts "Dog #{i}: This dog's name is #{dog.name} they are #{dog.age} years old, they are best described as  #{dog.description}, their breed is #{dog.breed}."
+    end
+  end
+
+  def generate_cats
+    20.times do |i|
+       cat = Cat.create!(
+        name: Faker::Creature::Cat.name,
+        age: Faker::Number.between(from: 1, to: 10),
+        description: Faker::Hipster.sentence,
+        breed: Faker::Creature::Cat.breed
+      )
+      puts "Cat #{i}: This cat's name is #{cat.name} they are #{cat.age} years old, they are best described as #{cat.description}, their breed is #{cat.breed}."
+    end
+  end
+end
