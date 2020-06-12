30fd2dc645da6586a85a1dbc4e6c655c0a1f9ea3
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-06-12 08:31:15 -0700

    update database schema to include two tables, one for cats and one for dogs each with columns for name, age, description and breed.

--- db/migrate/20200612152412_create_dogs.rb
+++ db/migrate/20200612152412_create_dogs.rb
@@ -1,6 +1,10 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
+      t.column :name, :string
+      t.column :age, :integer
+      t.column :description, :string
+      t.column :breed, :string
     end
   end
 end
--- db/migrate/20200612152421_create_cats.rb
+++ db/migrate/20200612152421_create_cats.rb
@@ -1,6 +1,10 @@
 class CreateCats < ActiveRecord::Migration[5.2]
   def change
     create_table :cats do |t|
+      t.column :name, :string
+      t.column :age, :integer
+      t.column :description, :string
+      t.column :breed, :string
     end
   end
 end
